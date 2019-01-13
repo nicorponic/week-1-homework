9a7dec6813181f2a3534b314ac62fafae257096e
nicorponic <n1co_frago@hotmail.com>
2019-01-13 15:13:21 +0100

    Hero object, Game logic functions and Resting are Done

---
+++ BB-HW/YOUR_JAVASCRIPT_HERE.js
@@ -0,0 +1,28 @@
+// Write your JS here
+
+const hero = {
+    name: "Joe",
+    heroic: true,
+    inventory: [],
+    health: 10,
+
+    weapon: {
+        type: "projectile",
+        damage: 2
+    }
+
+    
+};
+
+function rest(obj)  { 
+    obj.health = 10
+    return obj;
+}
+
+function pickUpItem()  {
+
+}
+
+function equipWeapon()   {
+
+}
