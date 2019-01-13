2b66f1882cd63572fe3671dbcbf1f8e069051fc4
nicorponic <n1co_frago@hotmail.com>
2019-01-13 18:29:51 +0100

    Clean code

--- BB-HW/YOUR_JAVASCRIPT_HERE.js
+++ BB-HW/YOUR_JAVASCRIPT_HERE.js
@@ -47,36 +47,19 @@ function displayStats(hero) {
     const heading3 = document.createElement("h2");
     const heading4 = document.createElement("h2");
 
-    //heading1.append(hero.name);
     
-    heading1.innerText = hero.name;
+    heading1.innerText = "hero name: " +hero.name;
     container.appendChild(heading1);
 
-    heading2.innerText = hero.health;
+    heading2.innerText = "Health: " +hero.health;
     container.appendChild(heading2);
 
-    heading3.innerText = hero.weapon.type;
+    heading3.innerText = "Weapon type: " +hero.weapon.type;
     container.appendChild(heading3);
 
-    heading4.innerText = hero.weapon.damage;
+    heading4.innerText = "Weapon damage: " +hero.weapon.damage;
     container.appendChild(heading4);
-    /*
-    heading2.innerHTML = hero.health;
-    heading3.innerHTML = hero.weapon.type;
-    heading4.innerHTML = hero.weapon.damage;
-    
-    
-    const heroStats = "Hero name: "+hero.name+
-                        ", Health status: "+hero.health+
-                        ", Weapon type: "+hero.weapon.type+
-                        ", Weapon damage: "+hero.weapon.damage;
-
-    paragraph.appendChild(heading);
-    
-    heading.appendChild(heroStats);
-
-    console.log(heroStats)
-    */
+ 
 }
 
 document.body.onload = displayStats(hero);
